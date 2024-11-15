3aa8b99faee6028c19fc17f530c59d57f81a30c6
Yusuf Zerdazi <yusuf@zerdazi.com>
2024-11-15 23:04:44 +0000

    directory

--- client/run.sh
+++ client/run.sh
@@ -13,10 +13,10 @@ cleanup() {
 trap cleanup SIGINT SIGTERM
 
 # Create and activate virtual environment if it doesn't exist
-if [ ! -d "venv" ]; then
+if [ ! -d ".venv" ]; then
     echo "Creating virtual environment..."
-    python3 -m venv venv
-    source venv/bin/activate
+    python3 -m venv .venv
+    source .venv/bin/activate
     pip install -r requirements.txt
 else
     source venv/bin/activate
