6ad3a6cac4b8947c04171632692920728ca9dd37
Yusuf Zerdazi <yusuf@zerdazi.com>
2024-11-16 14:21:53 +0000

    big

--- client/text_scroller.py
+++ client/text_scroller.py
@@ -1,20 +1,19 @@
 from PIL import Image, ImageDraw, ImageFont
-import textwrap
 
 class TextScroller:
     def __init__(self, width, height):
         self.width = width
         self.height = height
-        self.font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf', 16)
+        self.font = ImageFont.truetype('slkscr.ttf', 12)
         self.scroll_position = width
         self.current_text = None
         self.is_scrolling = False
-        self.scroll_speed = 2
+        self.scroll_speed = 4
         
-        self.y_position = 4
+        self.y_position = (self.height - 12) // 2
         
     def start_scroll(self, text):
-        self.current_text = text.upper()
+        self.current_text = text.upper().replace('\n', ' ').strip()
         self.scroll_position = self.width
         self.is_scrolling = True
         
