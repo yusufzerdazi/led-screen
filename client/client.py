d59f1b9f889fc99d6cbe17f8b5e27071a32cbc23
Yusuf Zerdazi <yusuf@zerdazi.com>
2024-11-16 12:51:22 +0000

    fix

--- client/client.py
+++ client/client.py
@@ -159,9 +159,10 @@ class Client:
         """Update the Hydra editor with new code by reloading with code parameter"""
         try:
             # Encode code as base64 and construct URL
+            print(code)
             new_code = base64.b64encode(code.encode('utf-8'))
             self.url = "http://localhost:5173?code=" + new_code.decode('utf-8')
-            
+
             print(self.url)
             
             # Load new URL
